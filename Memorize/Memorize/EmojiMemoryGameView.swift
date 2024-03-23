ba21145e4e14c7e785dc60ac55651de0f98cd65e
battian <39421745+battian@users.noreply.github.com>
2024-03-23 16:19:31 -0300

    Lecture 6: Layout, @ViewBuilder

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -9,13 +9,12 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
   @ObservedObject var viewModel: EmojiMemoryGame
+  private let aspectRatio: CGFloat = 2/3
   
   var body: some View {
     VStack {
-      ScrollView {
-        cards
-          .animation(.default, value: viewModel.cards)
-      }
+      cards
+        .animation(.default, value: viewModel.cards )
       Button("Shuffle") {
         viewModel.shuffle()
       }
@@ -23,18 +22,15 @@ struct EmojiMemoryGameView: View {
     .padding()
   }
   
-  var cards: some View {
-    LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-      ForEach(viewModel.cards) { card in
-        CardView(card)
-          .aspectRatio(2/3, contentMode: .fit)
-          .padding(4)
-          .onTapGesture {
-            viewModel.choose(card)
-          }
-      }
+  private var cards: some View {
+    AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
+      CardView(card)
+        .padding(4)
+        .onTapGesture {
+          viewModel.choose(card)
+        }
     }
-    .foregroundColor(.orange)
+    .foregroundColor(Color.orange)
   }
 }
 
@@ -48,24 +44,24 @@ struct CardView: View {
   var body: some View {
     ZStack {
       let base = RoundedRectangle(cornerRadius: 12)
-      
       Group {
         base.fill(.white)
-        base.stroke(lineWidth: 2)
+        base.strokeBorder(lineWidth: 2)
         Text(card.content)
           .font(.system(size: 200))
           .minimumScaleFactor(0.01)
-          .aspectRatio(1, contentMode: .fit)
-        
+          .aspectRatio(contentMode: .fit)
       }
-      .opacity(card.isFaceUp ? 1 : 0)
-      
-      base.opacity(card.isFaceUp ? 0 : 1)
+      .opacity(card.isFaceUp ? 1:0)
+      base.fill()
+        .opacity(card.isFaceUp ? 0:1)
     }
     .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
   }
 }
 
+
 #Preview {
   EmojiMemoryGameView(viewModel: EmojiMemoryGame())
 }
+
